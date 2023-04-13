64b7e9930fe0bd3c838c68a4507ccd1f981c5ea3
Tamiriam Fernandes <tamiriam.fernandes@hotmail.com>
2023-04-13 12:23:39 -0300

    adicionando biblioteca navigation

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
 import Home from './src/pages/Home';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
-  return <SafeAreaView style={{flex:1}}><Home /></SafeAreaView>
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar />
+      <NavigationContainer>
+        <Home />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
 }
 
 export default App;
